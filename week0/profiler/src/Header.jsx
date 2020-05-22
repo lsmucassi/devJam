2aff4be0e8d0fb78afc984aba570c376a9a6f88f
lsmucassi <lindasmucassi@gmail.com>
2020-05-22 18:17:54 +0200

    profiler body

--- week0/profiler/src/Header.jsx
+++ week0/profiler/src/Header.jsx
@@ -1,15 +1,56 @@
 import React from 'react'
 import { Toolbar, AppBar, Typography } from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add';
-import { makeStyles } from '@material-ui/styles'
+import { fade, makeStyles } from '@material-ui/core/styles'
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
 
-const useStyles = makeStyles(() => {
+const useStyles = makeStyles((theme) => ({
     typoStyles: {
         flex: 1
-    }
-})
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          width: '12ch',
+          '&:focus': {
+            width: '20ch',
+          },
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+          backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+          marginLeft: theme.spacing(1),
+          width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+}))
 
-const Header = ()=> {
+const Header = () => {
     const classes = useStyles();
 
     return (
@@ -20,6 +61,19 @@ const Header = ()=> {
                 Profiles | Admin
                 </Typography>
                 <AddIcon/>
+                <div className={classes.search}>
+                    <div className={classes.searchIcon}>
+                        <SearchIcon />
+                    </div>
+                    <InputBase
+                        placeholder="Search…"
+                        classes={{
+                            root: classes.inputRoot,
+                            input: classes.inputInput,
+                        }}
+                        inputProps={{ 'aria-label': 'search' }}
+                        />
+                    </div>
             </Toolbar>
         </AppBar>
         )
